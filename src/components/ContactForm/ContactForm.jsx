3c4f9c80787491b5bbb19ea6ac12e92bfc4ef563
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-12-01 13:05:00 +0200

    add files

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,56 @@
+import { Formik } from 'formik';
+import { FormContainer, Input, Button, Label } from './ContactForm.styled';
+import PropTypes from 'prop-types';
+
+const ContactForm = ({ contacts, onFormSubmit }) => {
+  const initialValues = {
+    name: '',
+    number: '',
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    resetForm();
+    onFormSubmit(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <FormContainer autoComplete="of">
+        <Label htmlFor="name">
+          Name:
+          <Input
+            type="text"
+            id="name"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label htmlFor="number">
+          Number:
+          <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </FormContainer>
+    </Formik>
+  );
+};
+
+ContactForm.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onFormSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
